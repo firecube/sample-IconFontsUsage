16eb22830b2009f2ceee14db882ecd771342454e
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2016-05-05 16:16:45 +0300

    clean up

--- platforms/ios/build/issue2055.build/Debug-iphonesimulator/issue2055.build/Objects-normal/i386/main.d
+++
@@ -1,8 +0,0 @@
-dependencies: \
-  /Users/niliev/Desktop/issues/issue_2055/platforms/ios/internal/main.m \
-  /Users/niliev/Desktop/issues/issue_2055/platforms/ios/internal/NativeScript/include/NativeScript.h \
-  /Users/niliev/Desktop/issues/issue_2055/platforms/ios/internal/NativeScript/include/TNSRuntime.h \
-  /Users/niliev/Desktop/issues/issue_2055/platforms/ios/internal/NativeScript/include/TNSRuntime+Diagnostics.h \
-  /Users/niliev/Desktop/issues/issue_2055/platforms/ios/internal/NativeScript/include/TNSRuntime+Inspector.h \
-  /Users/niliev/Desktop/issues/issue_2055/platforms/ios/internal/TNSExceptionHandler.h \
-  /Users/niliev/Desktop/issues/issue_2055/platforms/ios/internal/TNSDebugging.h
